7f79b3b022c882fbbd9f1a9dda9dc22437189937
David M <mullaned@gmail.com>
2018-10-04 14:43:37 +0100

    route parameters

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import User from './components/user/User.vue';
 import Home from './components/Home.vue';
 
 export const routes = [
-    {path: '/user', component: User},
+    {path: '/user/:id', component: User},
     {path: '', component: Home}
 ];
