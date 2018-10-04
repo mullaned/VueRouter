e5ba41e439ace58b5e5efef1080042583ca799ef
David M <mullaned@gmail.com>
2018-10-04 15:01:16 +0100

    setting up child routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,23 +3,14 @@
         <h1>The User Page</h1>
         <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
-        <p>Loaded ID: {{id}}</p>
+        <hr>
+        <router-view></router-view>
     </div>
 
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            id: this.$route.params.id
-        }
-    },
-    watch: {
-        '$route'(to, from){
-            this.id = to.params.id;
-        }
-    },
     methods: {
         navigateToHome(){
             this.$router.push({path: '/'});
